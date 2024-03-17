1fc4b1bc4258a8ec05083189a704891dff928556
lucas-geremias <geremiaslucas2012@gmail.com>
2024-03-17 10:28:55 -0300

    finalizandoteste

--- problemaC/Program.cs
+++ problemaC/Program.cs
@@ -1,23 +1,24 @@
-﻿string texto; 
-            int contador, cv; 
-            char[] vogais = { 'a','e','i','o','u' }; 
-            int total = 0;
+﻿string texto;
+int contador, cv;
+char[] vogais = { 'a', 'e', 'i', 'o', 'u' };
+int total = 0;
+int consoante = 1;
 
 
-            Console.WriteLine("Digita uma palavra: ");
-            texto = Console.ReadLine();
+Console.WriteLine("Digita uma palavra: ");
+texto = Console.ReadLine();
 
-            for (contador = 0; contador <= texto.Length - 1; contador++) 
-                for (cv = 0; cv <= 4; cv++) 
-                {
-                    if (Convert.ToChar(texto.Substring(contador, 1)) == vogais[cv])
+for (contador = 0; contador <= texto.Length - 1; contador++)
+    for (cv = 0; cv <= 4; cv++)
+    {
+        if (Convert.ToChar(texto.Substring(contador, 1)) == vogais[cv])
 
-                    {
-                        total++;
-                    }
+        {
+            total++;
+        }
+    }
+        consoante = contador - total;
+    
+Console.Write("Vogais " +total+ "." + "Consoantes"+consoante);
+Console.ReadKey();
 
-                }
-            Console.Write(total); 
-            Console.ReadKey();
-
-        
